44c8d9118a2c6f4dc7fd69da56f470b97a873e3a
yuhashmi <giogiosef97@gmail.com>
2021-09-09 13:33:25 +0100

    latest stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,28 +2,43 @@ module.exports = {
   siteMetadata: {
     title: 'Level Up Blog',
     description: 'A new blog!',
+    siteUrl: 'https://vibrant-poincare-f9e451.netlify.com',
   },
   plugins: [
     'gatsby-plugin-sitemap',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
-    'gatsby-plugin-sharp', 
+    'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Gatsby Blog',
+        short_name: 'GBlog',
+        start_url: '/',
+        background_color: '#524763',
+        theme_color: '#524763',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-offline',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/posts`,
-        name: 'posts'
-      }
+        name: 'posts',
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/images`,
-        name: 'images'
-      }
+        name: 'images',
+      },
     },
-    'gatsby-transformer-remark'
+    'gatsby-transformer-remark',
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-netlify`, // make sure to put last in the array
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,32 @@
 const path = require('path');
 
+
 exports.createPages = ({ graphql, actions }) => {
     const { createPage } = actions;
     return new Promise((resolve, reject) => {
         graphql(`
         {
             allMarkdownRemark {
-                edges {
-                        node {
-                    frontmatter{
+            edges {
+                node {
+                frontmatter {
                     slug
-                    }
-                    }
+                }
                 }
             }
             }
+        }    
         `).then(results => {
-            results.data.allMarkdownRemark.edges.forEach(({node}) => {
-                createPage({
-                    path: `/post${node.frontmatter.slug}`,
-                    component: path.resolve('./src/components/postLayout.js'),
-                    context: {
-                        slug: node.frontmatter.slug,
-                    }
-                });
-            })
-            resolve();
+        results.data.allMarkdownRemark.edges.forEach(({node}) => {
+            createPage({
+            path: `/posts${node.frontmatter.slug}`,
+            component: path.resolve('./src/components/postLayout.js'),
+            context: {
+                slug: node.frontmatter.slug,
+            }
+            });
+        })
+        resolve();
         })
     });
 }
