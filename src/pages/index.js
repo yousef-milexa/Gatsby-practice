1a29d16192a0875cc8936062a3423b05f4980bd2
yuhashmi <giogiosef97@gmail.com>
2021-09-13 14:56:06 +0100

    netlify cms saving path sorted

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import React from "react"
 import Listing from "../components/listing"
 
 import Layout from "../components/layout"
+import { Link } from "gatsby"
 
 const IndexPage = ({location}) => (
   <Layout location={location}>
     <h1>Home page</h1>
+    <Link to="/page-2/">Blog Page</Link>
   </Layout>
 )
 
