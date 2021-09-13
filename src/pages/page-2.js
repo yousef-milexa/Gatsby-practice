60c7a091dabff44595e5c3b3c4650e073755d60f
yuhashmi <giogiosef97@gmail.com>
2021-09-13 14:00:35 +0100

    netlify config file updated

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,82 +1,82 @@
-// import React from 'react'
-// import PropTypes from 'prop-types'
-// import Helmet from 'react-helmet'
-// import { graphql, Link } from 'gatsby'
-// import Layout from '../components/Layout'
-// import Content, { HTMLContent } from '../components/Content'
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
 
-// export const BlogPostTemplate = ({
-//   content,
-//   contentComponent,
-//   description,
-//   tags,
-//   title,
-//   helmet,
-// }) => 
+export const BlogPostTemplate = ({
+  content,
+  contentComponent,
+  description,
+  tags,
+  title,
+  helmet,
+}) => 
 
-// BlogPostTemplate.propTypes = {
-//   content: PropTypes.node.isRequired,
-//   contentComponent: PropTypes.func,
-//   description: PropTypes.string,
-//   title: PropTypes.string,
-//   helmet: PropTypes.object,
-// }
+BlogPostTemplate.propTypes = {
+  content: PropTypes.node.isRequired,
+  contentComponent: PropTypes.func,
+  description: PropTypes.string,
+  title: PropTypes.string,
+  helmet: PropTypes.object,
+}
 
-// const BlogPost = ({ data }) => {
-//   const { markdownRemark: post } = data
+const BlogPost = ({ data }) => {
+  const { markdownRemark: post } = data
 
-//   return (
-//     <Layout>
-//       <BlogPostTemplate
-//         content={post.html}
-//         contentComponent={HTMLContent}
-//         description={post.frontmatter.description}
-//         helmet={
-//           <Helmet titleTemplate="%s | Blog">
-//             <title>{`${post.frontmatter.title}`}</title>
-//             <meta
-//               name="description"
-//               content={`${post.frontmatter.description}`}
-//             />
-//           </Helmet>
-//         }
-//         title={post.frontmatter.title}
-//       />
-//     </Layout>
-//   )
-// }
+  return (
+    <Layout>
+      <BlogPostTemplate
+        content={post.html}
+        contentComponent={HTMLContent}
+        description={post.frontmatter.description}
+        helmet={
+          <Helmet titleTemplate="%s | Blog">
+            <title>{`${post.frontmatter.title}`}</title>
+            <meta
+              name="description"
+              content={`${post.frontmatter.description}`}
+            />
+          </Helmet>
+        }
+        title={post.frontmatter.title}
+      />
+    </Layout>
+  )
+}
 
-// BlogPost.propTypes = {
-//   data: PropTypes.shape({
-//     markdownRemark: PropTypes.object,
-//   }),
-// }
+BlogPost.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
 
-// export const pageQuery = graphql`
-//   query BlogPostByID($id: String!) {
-//     markdownRemark(id: { eq: $id }) {
-//       id
-//       html
-//       frontmatter {
-//         date(formatString: "MMMM DD, YYYY")
-//         title
-//         description
-//       }
-//     }
-//   }
-// `
+export const pageQuery = graphql`
+  query BlogPostByID($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        title
+        description
+      }
+    }
+  }
+`
 
-// export default BlogPost
+export default BlogPost
 
-import React from "react"
-import Listing from "../components/listing"
+// import React from "react"
+// import Listing from "../components/listing"
 
-import Layout from "../components/layout"
+// import Layout from "../components/layout"
 
-const SecondPage = ({location}) => (
-  <Layout location={location}>
-    <Listing />
-  </Layout>
-)
+// const SecondPage = ({location}) => (
+//   <Layout location={location}>
+//     <Listing />
+//   </Layout>
+// )
 
-export default SecondPage
+// export default SecondPage
