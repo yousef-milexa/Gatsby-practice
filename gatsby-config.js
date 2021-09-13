16474f4752fe34d759e1c2d2d28b5fe307764e01
yuhashmi <giogiosef97@gmail.com>
2021-09-13 12:04:38 +0100

    netlify stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-transformer-json',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -31,13 +32,41 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-transformer-remark`,
+        options: {
+          plugins: [
+            {
+              resolve: `gatsby-remark-images`,
+              options: {
+                maxWidth: 590,
+              },
+            },
+            {
+              resolve: `gatsby-remark-responsive-iframe`,
+              options: {
+                wrapperStyle: `margin-bottom: 1.0725rem`,
+              },
+            },
+            `gatsby-remark-prismjs`,
+            `gatsby-remark-copy-linked-files`,
+            `gatsby-remark-smartypants`,
+          ],
+    }
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/images`,
         name: 'images',
       },
     },
-    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/projects`,
+        name: 'projects',
+      },
+    },
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-netlify`, 
   ],
