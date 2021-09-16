5d7dfa62f6906dce6b160e1a011d80023e348a34
muralswallpaper <jonthornton@milexa.com>
2021-09-16 15:00:09 +0100

    Update to export file correctly

--- src/templates/default-page.js
+++ src/templates/default-page.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Layout from '../components/layout';
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
 
 const DefaultPageTemplate = () => (
-    <Layout>
-        <div>
-            <h2>title</h2>
-            <h3>description</h3>
-        </div>
-    </Layout>
+  <Layout>
+    <div>
+      <h2>title</h2>
+      <h3>description</h3>
+    </div>
+  </Layout>
 );
 
 export const DefaultPageTemplateQuery = graphql`
-query DefaultPageTemplateQuery($id: String!) {
-    datoCmsPage(id: {eq: $id}) {
-        heading
-        seoMetaTags {
-            tags
-        }
-        text
-        }
+  query DefaultPageTemplateQuery($id: String!) {
+    datoCmsPage(id: { eq: $id }) {
+      heading
+      seoMetaTags {
+        tags
+      }
+      text
     }
-    
-`
+  }
+`;
+export default DefaultPageTemplate;
